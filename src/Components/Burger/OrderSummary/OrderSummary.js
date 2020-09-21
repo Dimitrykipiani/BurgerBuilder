7facfe3b1fe2f01914c10609c6c8b314378c2d4a
Dimitry <dimitrykipiani@gmail.com>
2020-09-21 12:08:14 +0400

    IDK

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Auxil from '../../../Hoc/Auxil'
+import Button from '../../UI/Button/Button'
 
 const OrderSummary = (props) => {
     const ingredients = Object.keys(props.ingredients)
@@ -13,8 +14,10 @@ const OrderSummary = (props) => {
             <ul>
                 {ingredients}
             </ul>
-            <p>Total Price: {props.totalPrice}$</p>
+            <p>Total Price: {props.totalPrice.toFixed(2)}$</p>
             <p>Continue to chechout?</p>
+            <Button clicked={props.removeModal} btnType="Danger">Cancel</Button>
+            <Button clicked={props.purchace} btnType="Success">Continue</Button>
         </Auxil>
     )
 }
